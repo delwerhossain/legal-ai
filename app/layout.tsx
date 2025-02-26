d7d9938464e6ef9435dd0c8d8e7cb13b22ceb77e
syedhabib <syedmirhabib@gmail.com>
2025-02-26 15:56:45 +0600

    legal companion

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Navbar } from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "AinBondhu - Your Personal AinBondhu Assistant",
+  title: "AinBondhu - Your Legal Companion",
   description: "Get instant legal assistance with AinBondhu",
   icons: {
     icon: "/fav/favicon.ico",
